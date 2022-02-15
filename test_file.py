880808d16bf75cc48455a50be9be5f8ec5e54eb8
letsdoitfinally <70168305+letsdoitfinally@users.noreply.github.com>
2022-02-15 16:08:39 +0530

    Create test_file.py

---
+++ test_file.py
@@ -0,0 +1,11 @@
+import random
+
+
+print(f"Printing a randome number{random.random()}")
+print("Lets test")
+print("one more user added")
+print("fourth user")
+print("to check amount of credit debited")
+print("user 2 triggering build")
+print("user 3 is triggering build")
+print("user 5 is triggering build")
